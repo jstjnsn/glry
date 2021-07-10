462861f3af708202892bff9e30e14f91fb19cb9f
Joost Jansen <hello@joostjansen.me>
2021-07-10 02:08:24 +0200

    Load images from imagekit.io, adjust padding, update readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Glry
 
-This repository contains the code for a websie with a gallery of images.
+This repository contains the code for a websie with a gallery of images. Inspired by James Q Quick videos:
+
+- (Build an Image Gallery with Vanilla HTML, CSS, and JavaScript)[https://www.youtube.com/watch?v=vQc3EmFCWRw]
+- (DON'T Serve Unoptimized Images!!)[https://www.youtube.com/watch?v=pfHG6HL1GBw]
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const $ = document.querySelector.bind(document)
+const $$ = (s) => Array.from(document.querySelectorAll(s))
 
 const gallery = $('#gallery')
 const popup = $('#popup')
@@ -9,14 +10,15 @@ const selectedIndex = null
 
 for (let index of imageIndexes) {
   const image = document.createElement('img')
-  image.src = `/images/img${index}.png`
+  image.src = `https://ik.imagekit.io/qt6muek0hun/tr:w-600/img${index}.png`
   image.alt = `Gallery image ${index}`
   image.classList.add('galleryImage')
 
   image.addEventListener('click', () => {
     popup.style.transform = 'translateY(0)'
     popup.style.opacity = '1'
-    selectedImage.src = `/images/img${index}.png`
+    selectedImage.src = `https://ik.imagekit.io/qt6muek0hun/tr:w-800/img${index}.png`
+    selectedImage.srcSet = `https://ik.imagekit.io/qt6muek0hun/tr:w-400/img${index}.png 400w, https://ik.imagekit.io/qt6muek0hun/tr:w-800/img${index}.png 800w, https://ik.imagekit.io/qt6muek0hun/tr:w-1200/img${index}.png 1200w`
     selectedImage.alt = `Gallery image ${index}`
   })
 
@@ -28,6 +30,6 @@ popup.addEventListener('click', () => {
   setTimeout(() => {
     popup.style.transform = 'translateY(100%)'
   }, 250)
-  popup.src = ''
-  popup.alt = ''
+  selectedImage.src = ''
+  selectedImage.alt = ''
 })
